6f53eb1241db17710028440c943b58e5453a1cf5
AmirMohammad Kiani <01amirmkiani@gmail.com>
2025-11-17 20:39:21 +0330

    feat flower

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-import { openPopup , closePopup} from "./c";
-import { day_night_cycle } from "./b";
-import { levelUp } from "@workadventure/quests";
 
 console.log('Script started successfully');
 
@@ -15,35 +12,13 @@ WA.onInit().then(() => {
         WA.player.setOutlineColor(0, 119, 141);
     }
     
-    let currentZone: string;
-    WA.room.area.onEnter('fireplacezone').subscribe (() => {
-        //console.log('   uhiuiiuh        iuh iuiuh          uihuih');
-        currentZone = 'fireplacezone';
-        openPopup(currentZone, currentZone + 'Popup');
-    });
     
-    WA.room.area.onLeave('fireplacezone').subscribe(() => {
-        closePopup();
-    });
-    
-    //This function will automatically change day mode into night mode
-    //folders for the inputs: first day folder, second night folder.
-    //In each folder, the layers should be related to the state of day and night.
-    day_night_cycle("","");
-    
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
     
 }).catch(e => console.error(e));
 
-function levelup(){
-    levelUp("Up-time-meta",1);
-    //console.log('*************************Level up Up-time-meta');
-}
-
-setInterval(levelup, 1000);
 
 
 export {};
